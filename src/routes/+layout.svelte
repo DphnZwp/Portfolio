c7eca76a5372360e3e98abce1fa035183a779fe5
DphnZwp <zwuupdaphne@gmail.com>
2022-10-02 15:34:03 +0200

    🎉 Basis opzet en ontwerp

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+  import '../styles/global.css'
+</script>
+<header>
+  <nav class="menu">
+    <ul>
+      <li><a class="menu__link" href="/">Home</a></li>
+      <li><a class="menu__link" href="/">Blog</a></li>
+    </ul>
+  </nav>
+</header>
+
+<main>
+  <!-- Page content -->
+  <slot />
+</main>
+
+<style>
+</style>
