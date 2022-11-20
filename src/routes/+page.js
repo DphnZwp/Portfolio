b3de89cce7ca036853ec5f63f2f707c962ee0132
DphnZwp <zwuupdaphne@gmail.com>
2022-11-20 17:13:13 +0100

    hygraph data

---
+++ src/routes/+page.js
@@ -0,0 +1,25 @@
+export const load = async ({ fetch }) => {
+    const response = await fetch("https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clajji1v52wt701t67t8g8i7q/master", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          query: `{
+                    posts {
+                      highlightImage {
+                        url
+                      }
+                      title
+                      content {
+                        html
+                      }
+                    } 
+                  }`,
+        }),
+      });
+
+      const { data } = await response.json();
+
+      return data;
+}
