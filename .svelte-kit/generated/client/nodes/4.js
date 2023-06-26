db25fc18574a9d8f6b497f6246b4a791f731f6f9
DphnZwp <zwuupdaphne@gmail.com>
2023-06-26 23:22:47 +0200

    trying to solve vercel error

--- .svelte-kit/generated/client/nodes/4.js
+++ .svelte-kit/generated/client/nodes/4.js
@@ -1,1 +1,1 @@
-export { default as component } from "../../../../src/routes/detail/+page.svelte";
+export { default as component } from "../../../../src/routes/contact/+page.svelte";
--- .svelte-kit/generated/nodes/3.js
+++
@@ -1,3 +0,0 @@
-import * as shared from "..\\..\\..\\src\\routes\\blog\\+page.js";
-export { shared };
-export { default as component } from "..\\..\\..\\src\\routes\\blog\\+page.svelte";
